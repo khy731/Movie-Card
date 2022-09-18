dc4bf2f1c87421a3a650fb23f947325e9c2aab80
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-18 22:30:00 +0900

    영화 카드 리스트 컴포넌트
    /MyCard에 map으로 구현된다.

---
+++ src/pages/CardList.js
@@ -0,0 +1,11 @@
+const CardList = ( {data} ) => {
+    return(
+        <div className="CardList">
+            <h3>{data.name}에 {data.star}점을 주셨군요!</h3>
+            <p>{data.contents}</p>
+            <p>{data.date}</p>
+        </div>
+    );
+};
+
+export default CardList;
