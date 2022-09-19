ffb4f9a221ecc707904d7f1095f059ae1a4ab439
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-19 22:19:45 +0900

    더미데이터 대신 서버 데이터 GET
    - App.js에서 더미데이터를 받아 렌더링하지 않고 서버에 GET 요청을 보내 렌더링
    - Link로 카드 하나하나의 정보를 볼 수 있게 함

--- src/pages/MyCard.js
+++ src/pages/MyCard.js
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 import CardList from "./CardList";
 
-const MyCard = ( {data} ) => {
+const MyCard = ( ) => {
 
-    console.log(data);
     // 서버 띄워서 렌더링 하기
+    const data = useFetch('http://localhost:3001/movie');
 
     return(
         <div className="MyCard">
             <h2>총 {data.length}개의 카드가 있습니다.</h2>
             <div>
                 {data.map(v=> (
-                    <CardList id = {v.id} data={v}/>
+                    <Link className="AllCard" key={v.id} to={`/card/${v.id}`}>
+                        <CardList data={v} />
+                    </Link>
                 ))}
             </div>
         </div>
