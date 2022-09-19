ee9574d06b410d5c0f6bf0ab1b9519146ecbc0d0
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-19 22:18:01 +0900

    간단 수정
    줄바꿈만 수정. 별 내용 없음

--- src/pages/CardList.js
+++ src/pages/CardList.js
@@ -1,9 +1,10 @@
 const CardList = ( {data} ) => {
+
     return(
         <div className="CardList">
-            <h3>{data.name}에 {data.star}점을 주셨군요!</h3>
-            <p>{data.contents}</p>
-            <p>{data.date}</p>
+                <h3>{data.name}에 {data.star}점을 주셨군요!</h3>
+                <p>{data.contents}</p>
+                <p>{data.date}</p>
         </div>
     );
 };
