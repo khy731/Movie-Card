45c8dd4818fe84dbaa6803f28d85dcd4bb92c0f3
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-21 20:26:50 +0900

    요소별 className 추가
    grid layout 스타일을 위한 클래스를 추가했다

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <header className="Header">
-            <h2>
+            <h2 className='leftChild'>
                 <Link to={-1}>{'<'}</Link>
             </h2>
-            <h2>
+            <h2 className='centerChild'>
                 <Link to={'/'}>Make A Card</Link>
             </h2>
-            <h2>
+            <h2 className='rightChild'>
                 <Link to={'/mycard'}>my card</Link>
             </h2>
         </header>
