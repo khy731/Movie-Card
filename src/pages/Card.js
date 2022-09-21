42f8dbad99cf15ba2c13aa513109c005fe572018
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-21 20:27:39 +0900

    삭제, 수정 기능 구현
    - 서버로부터 PATCH, DELETE
    - 서버 사이드 상태와 클라이언트 사이드 상태를 구분할 필요가 있어보임

--- src/pages/Card.js
+++ src/pages/Card.js
@@ -1,16 +1,82 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 
 const Card = ( ) => {
 
     const { id } = useParams();
+
     const matchedData = useFetch(`http://localhost:3001/movie/${id}`);
+
+    const [isEdit, setIsEdit] = useState(false);
+    const [localContents, setLocalContents] = useState(matchedData.contents);
+    const navigate = useNavigate();
+
+
+    const toggleIsEdit = () => {
+        setIsEdit(!isEdit);
+        setLocalContents(matchedData.contents);
+    }
+    const handleTextarea = e => {
+        setLocalContents(e.target.value);
+    };
+
+    const editCard = () => {
+        fetch(`http://localhost:3001/movie/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                contents: localContents,
+                date: new Date().toLocaleString(),
+            }),
+        })
+        .then(res => {
+            if(res.ok) {
+                toggleIsEdit();
+            }
+        })
+    };
+
+    const removeCard = () => {
+        if (window.confirm("삭제하시겠습니까?")) {
+            fetch(`http://localhost:3001/movie/${id}`, {
+                method: "DELETE",
+            })
+            .then(res => {
+                if(res.ok) {
+                    alert("삭제 완료!");
+                    navigate("/mycard");
+                }
+            })
+        }
+    }
 
     return (
             <div className="Card">
                 <h3>{matchedData.name}에 {matchedData.star}점을 주셨군요!</h3>
-                <p>{matchedData.contents}</p>
+                <div>
+                    { isEdit ? 
+                        <textarea value={localContents} onChange={handleTextarea}></textarea>
+                        :
+                        <p>{matchedData.contents}</p>    
+                    }
+                </div>
                 <p>{matchedData.date}</p>
+                <div>
+                    { isEdit ?
+                    <div>
+                        <button onClick={toggleIsEdit}>수정 취소</button>
+                        <button onClick={editCard}>수정 완료</button>
+                    </div>
+                        :
+                    <div>
+                        <button onClick={toggleIsEdit}>수정하기</button>
+                        <button onClick={removeCard}>삭제하기</button>
+                    </div> 
+                    }
+                </div>
             </div>
     );
 };
