ce752d61b3252d91bdad514a823a1e59d94ee187
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-26 21:51:34 +0900

    모달 구현 : 모달창 렌더링 하기
    - Link -> button으로 변경
    - isModal로 모달 on/off 상태 관리
    - showModal로 모달창 렌더링, closeModal로 모달창 렌더링 하지 않기
    - closeModal은 Modal 컴포넌트에 props로 전달

--- src/pages/MyCard.js
+++ src/pages/MyCard.js
@@ -1,23 +1,36 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import CardList from "./CardList";
+import Modal from "./Modal";
 
 const MyCard = ( ) => {
 
     // 서버 띄워서 렌더링 하기
     const data = useFetch('http://localhost:3001/movie');
 
+    // 모달 상태 관리
+    const [isModal, setIsModal] = useState(false);
+
+    const showModal = () => {
+        setIsModal(true);
+    };
+    
+    const closeModal = () => {
+        setIsModal(false);
+    }
+
     return(
         <div>
         {data.length < 1 ? <h1>Loading...</h1> :
             <div className="MyCard">
+                { isModal && <Modal closeModal={closeModal} /> }
                 <h2>총 {data.length}개의 카드가 있습니다.</h2>
                 <div>
                     {data.map(v=> (
-                        <Link className="AllCard" key={v.id} to={`/card/${v.id}`}>
+                        <button className="AllCard" key={v.id} onClick={showModal}>
                             <CardList data={v} />
-                        </Link>
+                        </button>
                     ))}
             </div>
         </div>
