10405d2cd4d190aee8e7887473ef8648baaf205d
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-26 18:39:03 +0900

    응답이 느릴 시  "Loading..." 렌더링

--- src/pages/MyCard.js
+++ src/pages/MyCard.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import CardList from "./CardList";
@@ -8,16 +9,20 @@ const MyCard = ( ) => {
     const data = useFetch('http://localhost:3001/movie');
 
     return(
-        <div className="MyCard">
-            <h2>총 {data.length}개의 카드가 있습니다.</h2>
-            <div>
-                {data.map(v=> (
-                    <Link className="AllCard" key={v.id} to={`/card/${v.id}`}>
-                        <CardList data={v} />
-                    </Link>
-                ))}
+        <div>
+        {data.length < 1 ? <h1>Loading...</h1> :
+            <div className="MyCard">
+                <h2>총 {data.length}개의 카드가 있습니다.</h2>
+                <div>
+                    {data.map(v=> (
+                        <Link className="AllCard" key={v.id} to={`/card/${v.id}`}>
+                            <CardList data={v} />
+                        </Link>
+                    ))}
             </div>
         </div>
+        }
+    </div>
     );
 };
 
