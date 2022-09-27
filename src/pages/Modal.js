727e638aca3e6db6017fcabe750d2368154f50c6
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-27 21:43:19 +0900

    모달 컴포넌트
    기존 Card의 기능을 전부 mv

---
+++ src/pages/Modal.js
@@ -0,0 +1,93 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Modal = ( {setIsModal, data} ) => {
+
+    const [matchedData, setMatchedData] = useState([]);
+    const [isEdit, setIsEdit] = useState(false);
+    const [localContents, setLocalContents] = useState(matchedData.contents);
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        setMatchedData(data);
+    }, []);
+
+    const toggleIsEdit = () => {
+        setIsEdit(!isEdit);
+        setLocalContents(matchedData.contents);
+    };
+    const handleTextarea = e => {
+        setLocalContents(e.target.value);
+    };
+    const handleClick = () => {
+        setIsModal(false);
+    };
+
+    const editCard = () => {
+        fetch(`http://localhost:3001/movie/${data.id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: JSON.stringify({
+                contents: localContents,
+                date: new Date().toLocaleString(),
+            }),
+        })
+        .then(res => {
+            if(res.ok) {
+                toggleIsEdit();
+                setMatchedData({
+                    ...matchedData,
+                    contents: localContents,
+                });
+            }
+        })
+    };
+
+    const removeCard = () => {
+        if (window.confirm("삭제하시겠습니까?")) {
+            fetch(`http://localhost:3001/movie/${data.id}`, {
+                method: "DELETE",
+            })
+            .then(res => {
+                if(res.ok) {
+                    alert("삭제 완료!");
+                    navigate("/mycard");
+                }
+            })
+        }
+    };
+
+    return (
+        <div className="Modal">
+            <div className="Card">
+                <button onClick={handleClick}>X</button>
+                <h3>{matchedData.name}에 {matchedData.star}점을 주셨군요!</h3>
+                <div>
+                    { isEdit ? 
+                        <textarea value={localContents} onChange={handleTextarea}></textarea>
+                        :
+                        <p>{matchedData.contents}</p>    
+                    }
+                </div>
+                <p>{matchedData.date}</p>
+                <div>
+                    { isEdit ?
+                    <div>
+                        <button onClick={toggleIsEdit}>수정 취소</button>
+                        <button onClick={editCard}>수정 완료</button>
+                    </div>
+                        :
+                    <div>
+                        <button onClick={toggleIsEdit}>수정하기</button>
+                        <button onClick={removeCard}>삭제하기</button>
+                    </div> 
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;
