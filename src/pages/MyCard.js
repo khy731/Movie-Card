00a14b80c581a829ae27a7f00a2ba74803d7fcf7
DESKTOP-5TQQEGA\강혜윤 <khy00731@gmail.com>
2022-09-27 21:42:53 +0900

    모달 데이터 업데이트
    CardList의 개별 데이터를 상태로 관리하여 모달 컴포넌트에 전달

--- src/pages/MyCard.js
+++ src/pages/MyCard.js
@@ -1,35 +1,28 @@
 import { useState } from "react";
-import { useSearchParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
 import CardList from "./CardList";
 import Modal from "./Modal";
 
 const MyCard = ( ) => {
 
-    // 서버 띄워서 렌더링 하기
     const data = useFetch('http://localhost:3001/movie');
 
-    // 모달 상태 관리
     const [isModal, setIsModal] = useState(false);
-
-    const showModal = () => {
-        setIsModal(true);
-    };
-    
-    const closeModal = () => {
-        setIsModal(false);
-    }
+    const [selectedData, setSelectedData] = useState({});
 
     return(
         <div>
         {data.length < 1 ? <h1>Loading...</h1> :
             <div className="MyCard">
-                { isModal && <Modal closeModal={closeModal} /> }
+                {isModal && <Modal data={selectedData} setIsModal={setIsModal} />}
                 <h2>총 {data.length}개의 카드가 있습니다.</h2>
                 <div>
                     {data.map(v=> (
-                        <button className="AllCard" key={v.id} onClick={showModal}>
-                            <CardList data={v} />
+                        <button className="AllCard" key={v.id} onClick={() => {
+                            setIsModal(true);
+                            setSelectedData(v);
+                        }}>
+                            <CardList data={v}/>
                         </button>
                     ))}
             </div>
